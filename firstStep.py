e3ca421e6dbe9165f98777cc6c44f73eb82565de
David Shamoev <shamoevdavid04@gmail.com>
2025-01-28 12:59:17 +0500

    Не доделал последнюю задачу 2го модуля

--- firstStep.py
+++ firstStep.py
@@ -15,67 +15,9 @@
 # y = 10
 # print((y > (x * x)) or (y >= (2 * x)) and x < y)
 #
-# n = int(input())
-# word = "программист"
-#
-# if (n % 2 == 0 or n % 3 == 0 or n % 4 == 0) and (n < 5 or n > 20) and n != 0:
-#     if n % 10 == 1:
-#         end = ""
-#     else:
-#         end = "а"
-# elif n == 1:
-#     end = ""
-# else:
-#     end = "ов"
-#
-# print(n, word + end)
 
-# n = input()
-# e = int(n[-1])
-# word = "программист"
-#
-# if len(n) > 2:
-#     ed = n[-2:]
-#     if int(ed) == 11 or int(ed) == 0:
-#         end = "ов"
-#
-# if n == "1" or (e == 1 and int(n) != 11):
-#     end = ""
-#
-# elif e in [0, 5, 6, 7, 8, 9] or int(n[-2:]) in [12, 13] or int(n) in [12, 13] or int(n) == 11 or int(n) == 14:
-#     end = "ов"
-#
-# else:
-#     end = "а"
-#
-# print(n, word + end)
-
-
-
-# ticket = input()
-#
-# count_left = 0
-# count_right = 0
-#
-# for i in ticket[0:3]:
-#     count_left += int(i)
-#
-# for i in ticket[3:6]:
-#     count_right += int(i)
-#
-# if count_left == count_right:
-#     print("Счастливый")
-# else:
-#     print("Обычный")
-
-
-a = int(input())
-b = int(input())
-i = 1
-
-while i <= (a * b) and i != 0:
-    if i % a == 0 and i % b == 0:
-        print(i)
-        i = 0
-    else:
-        i += 1
+# Генерация списков
+# a = [0] * 5
+# a = [0 for i in range(5)]
+# a = [i*i for i in range(5)]
+# a = [int(i) for i in input().split()]
--- test.py
+++ test.py
@@ -1,2 +1,234 @@
-print("hello")
-print("hello")
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+#
+#
+# for i in range(c, d+1):
+#     print('\t', i, end='\t')
+#
+# for j in range(a, b + 1):
+#     print('\n', j)
+
+#
+# a = int(input())
+# b = int(input())
+#
+# c = 0
+# sum = 0
+#
+# for i in range(a, b+1):
+#     if i % 3 == 0:
+#         c += 1
+#         sum += i
+#
+# print(sum / c)
+
+
+# genom = input().lower()
+# c = 0
+#
+# for s in genom:
+#     if s == "c" or s == "g":
+#         c += 1
+#
+# print((c / len(genom)) * 100)
+
+
+# s = "abccba1"
+#
+# if s == s[::-1]:
+#     print("yes")
+# else:
+#     print("no")
+
+# s = input()
+# c = 0
+#
+# for i in range(len(s)):
+#     if s[i] == s[i+1]:
+#         c += 1
+#     else:
+#         c = 0
+#
+#     print(s[i],c, end ="")
+
+
+# s = input()
+# i = 0
+# c = 1
+#
+# while i < len(s) - 1:
+#     if s[i] == s[i+1]:
+#         c += 1
+#     else:
+#         print(s[i], c, sep="", end="")
+#         c = 1
+#     i += 1
+
+
+# s = input()
+# i = 0
+# c = 1
+#
+# if len(s) == 1:
+#     print(s, c, sep="")
+#
+# while i < len(s) - 1:
+#     if s[i] == s[i+1]:
+#         c += 1
+#         if (len(s) - 1) - i == 1:
+#             print(s[i], c, sep="", end="")
+#     else:
+#         print(s[i], c, sep="", end="")
+#         c = 1
+#         if (len(s) - 1) - i == 1:
+#             print(s[i+1], c, sep="", end="")
+#     i += 1
+
+
+# a = [int(i) for i in input().split()]
+# new = [0] * len(a)
+#
+# for i in range(len(a)):
+#     if len(a) == 1:
+#         print(a[0])
+#     elif i == 0:
+#         new[0] = a[-1] + a[i + 1]
+#     elif i == len(a) - 1:
+#         new[-1] = a[-2] + a[0]
+#     else:
+#         new[i] = a[i+1] + a[i-1]
+#
+# if len(a) != 1:
+#     for item in new:
+#         print(item, end=' ')
+
+
+
+# a = [int(i) for i in input().split()]
+# c = 0
+# new = []
+#
+# for i in range(len(a)):
+#     for j in range(len(a)):
+#         if a[i] == a[j]:
+#             c += 1
+#             if a[i] not in new and c > 1:
+#                 new.append(a[i])
+#                 c = 0
+#
+#
+# print(new)
+
+
+# a = [int(i) for i in input().split()]
+# new = []
+#
+# for i in range(len(a)):
+#     if a[i] in a:
+#         new.append(a[i])
+# print(new)
+
+#
+# n, m = (int(i) for i in input().split())
+# a = [[0 for j in range(m)]for i in range(n)]
+
+# a = []
+# inp =input()
+# while inp != "end":
+#     mas = [int(i) for i in inp.split()]
+#     a.append(mas)
+#     inp = input()
+#
+# n = len(a)
+# m = len(a[0])
+# print(f'Матрица размером {n} x {m}')
+#
+# a = []
+# inp =input()
+# while inp != "end":
+#     mas = [int(i) for i in inp.split()]
+#     a.append(mas)
+#     inp = input()
+
+# n = len(a)
+# m = len(a[0])
+# print(f'Матрица размером {n} x {m}')
+
+
+
+
+# mas = [int(i) for i in input().split()]
+# n = len(mas)
+# res = [0 for j in range(n)]
+# print(f'res: {res}')
+#
+# for i in range(n):
+#     if i == 0:
+#         res[i] = mas[i + 1] + mas[-1]
+#     elif i == n - 1:
+#         res[i] = mas[i - 1] + mas[0]
+#     else:
+#         res[i] = mas[i - 1] + mas[i + 1]
+#
+# print(res)
+#
+
+# --------------------------модуль основного решения-----------------------------------------------
+# a = []
+# inp =input()
+# while inp != "end":
+#     mas = [int(i) for i in inp.split()]
+#     a.append(mas)
+#     inp = input()
+#
+# n = len(a)
+# m = len(a[0])
+# res = [[0 for i in range(m)] for j in range(n)]
+# print(f'res: {res}')
+#
+# for i in range(n):
+#     for j in range(m):
+#         res[i][j] = a[i-1][j] + a[i+1][j] + a[i][j-1] + a[i][j+1]
+# -------------------------------------------------------------------------------------------
+# a = []
+# inp =input()
+# while inp != "end":
+#     mas = [int(i) for i in inp.split()]
+#     a.append(mas)
+#     inp = input()
+#
+# n = len(a)
+# m = len(a[0])
+# res = [[0 for i in range(m)] for j in range(n)]
+#
+# for i in range(n):
+#     for j in range(m):
+#         res[i][j] = a[i-1][j] + a[i-m+1][j] + a[i][j-1] + a[i][j-m+1]
+#
+# for s in res:
+#     for c in s:
+#         print(c, end=' ')
+#     print()
+
+#выше решение
+a = []
+inp =input()
+while inp != "end":
+    mas = [int(i) for i in inp.split()]
+    a.append(mas)
+    inp = input()
+
+n = len(a)
+m = len(a[0])
+res = [[0 for i in range(m)] for j in range(n)]
+
+for i in range(n):
+    for j in range(m):
+        res[i][j] = a[i-1][j] + a[i-n+1][j] + a[i][j-1] + a[i][j-m+1]
+
+for s in res:
+    for c in s:
+        print(c, end=' ')
+    print()
